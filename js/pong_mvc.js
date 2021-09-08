ec1bd643e6f9b672ce6d32943ee118af3e69cd8b
ajcoque <84734358+ajcoque@users.noreply.github.com>
2021-09-08 17:18:35 -0500

    Se agrego metodo para generar rebotes de la pelota con las barras, archivo modificado pong_mvc.js

--- js/pong_mvc.js
+++ js/pong_mvc.js
@@ -46,6 +46,26 @@
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+
+        get width() {
+            return this.radius * 2;
+        },
+
+        get height() {
+            return this.radius * 2;
+        },
+
+        collision: function (bar) {
+            //reacciona a la colicion con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 
@@ -103,13 +123,48 @@
                 draw(this.ctx, el);
             };
         },
+
+        //Funcion que permite establecer si la bola choco con la barra
+        check_collisions: function () {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
+
         play: function () {
             if (this.board.playing) {
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
+    }
+
+    function hit(a, b) {
+        //revisa si a coliciona con b
+        //se puede utilizar siempre y cuando se cumplan unas condiciones
+        var hit = false;
+        //Colisiones horizontales
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            //colisiones verticales
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+        //colision de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+        //colision de b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
     }
 
     function draw(ctx, element) {
